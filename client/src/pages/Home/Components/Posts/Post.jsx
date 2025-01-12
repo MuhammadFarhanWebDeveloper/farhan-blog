a5c07be0667eb94e5370cf30b1ce84789af3c681
Muhammad Farhan <muhammadfarhan3789076@gmail.com>
2025-01-12 15:07:54 +0500

    fixed

--- client/src/pages/Home/Components/Posts/Post.jsx
+++ client/src/pages/Home/Components/Posts/Post.jsx
@@ -9,7 +9,7 @@ function Post({ post, deletePost }) {
 
   return (
     <div className="relative md:w-[90%] w-[100%] flex border  flex-col md:flex-row p-1 rounded">
-      {post.userid == currentUser?._id && (
+      {post.user._id == currentUser?._id && (
         <span
           onClick={() => {
             deletePost(post._id);
